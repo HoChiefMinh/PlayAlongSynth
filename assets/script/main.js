845582a80c03306b543d8cd9effafc24f33e5686
Andrew Stiles <ndrwstls544@gmail.com>
2019-07-19 01:17:36 -0400

    created keyboard

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,15 +1,10 @@
-//create a synth and connect it to the master output (your speakers)
-var synth = new Tone.Synth().toMaster()
-
-//play a middle 'C' for the duration of an 8th note
-synth.triggerAttackRelease('C4', '8n')
-
-document.querySelector('tone-keyboard').addEventListener('noteon', e => {
-    synth.triggerAttack(e.detail.name)
-  })
-  
-  document.querySelector('tone-keyboard').addEventListener('noteoff', e => {
-    synth.triggerRelease()
-  })
-
-  
+var keyboard = new QwertyHancock({
+  id: 'keyboard',
+  width: 600,
+  height: 150,
+  octaves: 2,
+  startNote: 'A3',
+  whiteNotesColour: 'white',
+  blackNotesColour: 'black',
+  hoverColour: '#f3e939'
+});
