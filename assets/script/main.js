b62a0c063cb502447ea1915108b63376e53728e6
Andrew Stiles <ndrwstls544@gmail.com>
2019-07-18 21:23:09 -0400

    initial synth

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,15 @@
-var array1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+//create a synth and connect it to the master output (your speakers)
+var synth = new Tone.Synth().toMaster()
+
+//play a middle 'C' for the duration of an 8th note
+synth.triggerAttackRelease('C4', '8n')
+
+document.querySelector('tone-keyboard').addEventListener('noteon', e => {
+    synth.triggerAttack(e.detail.name)
+  })
+  
+  document.querySelector('tone-keyboard').addEventListener('noteoff', e => {
+    synth.triggerRelease()
+  })
+
+  
